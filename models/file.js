1e0b80052c00bda760103d1304e2f35d19aa195f
shubham-1809 <shubhamchouksey1809@gmail.com>
2023-12-23 15:32:08 +0530

    added files

---
+++ models/file.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const fileSchema = new Schema({
+    filename: { type: String, required: true },
+    path: { type: String, required: true },
+    size: { type: Number, required: true },
+    uuid: { type: String, required: true },
+    sender: { type: String, required: false },
+    receiver: { type: String, required: false },
+    collegeName: { type: String, required: false },
+    studentName: { type: String, required: false },
+    rollNumber: { type: int, required: false },
+    branchName: { type: String, required: false },
+    section: { type: int, required: false },
+    projectType: { type: String, required: false },
+}, { timestamps: true });
+
+module.exports = mongoose.model('File', fileSchema);
