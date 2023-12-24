f3edfc3f774a93f3e60e01ae4c42ba6a2806a905
shubham-1809 <shubhamchouksey1809@gmail.com>
2023-12-24 11:51:45 +0530

    added files

--- services/emailTemplate.js
+++ services/emailTemplate.js
@@ -114,8 +114,8 @@ module.exports = ({emailFrom, downloadLink, size, expires, collegeName, studentN
                                 <tr>
                                 <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;">
                                     <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;">Hi there,</p>
-                                    <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;"><b>${emailFrom}</b> (${studentName}) from college ${collegeName}, branch ${branch}${section} has shared a ${projectType} file with you. Student enrollment number: <b>${rollNumber}.</b></p>
-                                    <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;">${size} in total ・ Expires in ${expires}</p>
+                                    <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;"><b>${emailFrom}</b> (${studentName}) from college <b>${collegeName}</b>, branch <b>${branch}${section}</b> has shared a ${projectType} Project file with you. Student enrollment number: <b>${rollNumber}.</b></p>
+                                    <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;">${size} in total ・ Download Link Expires in ${expires}</p>
                                     <table border="0" cellpadding="0" cellspacing="0" class="btn btn-primary" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; box-sizing: border-box;">
                                     <tbody>
                                         <tr>
@@ -147,13 +147,13 @@ module.exports = ({emailFrom, downloadLink, size, expires, collegeName, studentN
                         <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;">
                             <tr>
                             <td class="content-block" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; font-size: 12px; color: #999999; text-align: center;">
-                                <span class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">inShare inc. main street 29, Moscow</span>
-                                <br> Want to share a file? <a href="http://localhost:3000" style="text-decoration: underline; color: #999999; font-size: 12px; text-align: center;">inShare</a>.
+                                <span class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">File Flows Service, Jabalpur, India</span>
+                                <br> Want to share a file? <a href="https://fileflows.onrender.com" style="text-decoration: underline; color: #999999; font-size: 12px; text-align: center;">FileFlows</a>.
                             </td>
                             </tr>
                             <tr>
                             <td class="content-block powered-by" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; font-size: 12px; color: #999999; text-align: center;">
-                                Powered by <a href="https://www.youtube.com/channel/UCo9xTRmg1SqQ5JSsA2fAgJw" style="color: #999999; font-size: 12px; text-align: center; text-decoration: none;">Coders Gyan</a>.
+                                Powered by <a href="https://shubham-1809.github.io/My-Personal-Portfolio/" style="color: #999999; font-size: 12px; text-align: center; text-decoration: none;">Engineer's Palette</a>.
                             </td>
                             </tr>
                         </table>
